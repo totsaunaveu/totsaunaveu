1017dc028a0949511ddee53a748c1e5a93b469cf
Pau March <paumrch94@gmail.com>
2022-10-09 20:05:55 +0200

    next-config

--- next.config.js
+++ next.config.js
@@ -7,6 +7,13 @@ if (!process.env.WORDPRESS_API_URL) {
 
 /** @type {import('next').NextConfig} */
 module.exports = {
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
   images: {
     domains: [
       process.env.WORDPRESS_API_URL.match(/(?!(w+)\.)\w*(?:\w+\.)+\w+/)[0], // Valid WP Image domain.
