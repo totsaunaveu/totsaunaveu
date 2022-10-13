cab1e9d5154a05a60b838768cb05d65f46096557
Pau March <paumrch94@gmail.com>
2022-10-13 14:26:03 +0200

    OG works fine

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,6 +5,7 @@ import MoreStories from '../components/more-stories'
 import HeroPost from '../components/hero-post'
 import Intro from '../components/intro'
 import Layout from '../components/layout'
+import { HOME_OG_IMAGE_URL } from "../lib/constants";
 import { getAllPostsForHome } from '../lib/api'
 
 export default function Index({ allPosts: { edges }, preview }) {
@@ -15,6 +16,7 @@ export default function Index({ allPosts: { edges }, preview }) {
     <Layout preview={preview}>
       <Head>
         <title>DELS VALENCIANS</title>
+        <meta property="og:image" content={HOME_OG_IMAGE_URL} />
       </Head>
       <Container>
         <Intro />
