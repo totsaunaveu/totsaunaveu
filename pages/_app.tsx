a02951a423a16b37e7184f710f910cfe03402a4c
Pau March <paumrch94@gmail.com>
2022-10-17 23:35:37 +0200

    Analytics 2nd Try

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,25 @@
 import { AppProps } from "next/app";
 import "../styles/index.css";
-import { GoogleAnalytics } from "nextjs-google-analytics";
+import Script from "next/script";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
-      <GoogleAnalytics trackPageViews />
+      <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+      />
+
+      <Script id="google-analytics" strategy="lazyOnload">
+        {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+              page_path: window.location.pathname,
+            });
+                `}
+      </Script>
       <Component {...pageProps} />
     </>
   );
