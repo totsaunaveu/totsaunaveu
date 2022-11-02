d9033d142673b4b8bd546ad9093f2c21f3cb4950
totsaunaveu <totsaunaveu2022@gmail.com>
2022-11-02 12:48:59 +0100

    Analytics OK

--- pages/index.tsx
+++ pages/index.tsx
@@ -38,6 +38,23 @@ export default function Index({ allPosts: { edges }, allFeaturedPosts: { edges: 
       <Head>
         <title>Tots a una veu — La veu dels valencians</title>
         <meta property="og:image" content={HOME_OG_IMAGE_URL} />
+         {/* Global Site Tag (gtag.js) - Google Analytics */}
+         <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+              page_path: window.location.pathname,
+            });
+          `,
+            }}
+          />
       </Head>
       <Container>
         <Intro />
